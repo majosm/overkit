79fa1661050c1b05b557e14aa9deb57ec22b992e
Matthew Smith <mjsmith6@illinois.edu>
2019-03-08 22:23:32 -0600

    minor fix

--- src/core/Connectivity.cpp
+++ src/core/Connectivity.cpp
@@ -94,7 +94,7 @@ void CreateConnectivity(connectivity &Connectivity, int NumDims, core::comm Comm
   MPI_Barrier(Connectivity.Comm_);
 
   core::LogStatus(*Connectivity.Logger_, Connectivity.Comm_.Rank() == 0, 0, "Created connectivity "
-    " %s.", Connectivity.Name_);
+    "%s.", Connectivity.Name_);
 
 }
 
