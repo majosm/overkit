a43a6e86cf34dea4d2a27a981592eb894ca8c967
Matthew Smith <mjsmith6@illinois.edu>
2019-03-19 14:18:17 -0500

    separated mpi_test and mpi_printer into two header files

--- tests/UnitTestMain.cpp
+++ tests/UnitTestMain.cpp
@@ -1,8 +1,9 @@
 // Copyright (c) 2018 Matthew J. Smith and Overkit contributors
 // License: MIT (http://opensource.org/licenses/MIT)
 
+#include "tests/MPIPrinter.hpp"
+
 #include "gtest/gtest.h"
-#include "MPITest.hpp"
 
 #include <ovk/core/TextProcessing.hpp>
 
