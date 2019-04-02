0ba56a2a93aa47db44a0fa49544b17fc1d0a88d9
Matthew Smith <mjsmith6@illinois.edu>
2019-04-02 16:56:18 -0500

    added ForEach for looping over N-dimensional interval

---
+++ src/core/ForEach.hpp
@@ -0,0 +1,98 @@
+// Copyright (c) 2018 Matthew J. Smith and Overkit contributors
+// License: MIT (http://opensource.org/licenses/MIT)
+
+#ifndef OVK_CORE_FOR_EACH_HPP_INCLUDED
+#define OVK_CORE_FOR_EACH_HPP_INCLUDED
+
+#include <ovk/core/Constants.hpp>
+#include <ovk/core/Elem.hpp>
+#include <ovk/core/Global.hpp>
+#include <ovk/core/Interval.hpp>
+#include <ovk/core/TypeSequence.hpp>
+#include <ovk/core/TypeTraits.hpp>
+
+#include <type_traits>
+
+namespace ovk {
+namespace core {
+
+namespace for_each_internal {
+
+namespace is_callable_with_tuple_elements_internal {
+template <typename FRef, typename... TupleElementTypes> constexpr bool Helper(type_sequence<
+  TupleElementTypes...>) {
+  return IsCallableWith<FRef, TupleElementTypes...>();
+}
+}
+template <typename FRef, typename T, int N> constexpr bool IsCallableWithTupleElements() {
+  return is_callable_with_tuple_elements_internal::Helper<FRef>(
+    repeated_type_sequence_of_size<T, N>());
+}
+
+template <int N, int I, array_layout Layout> struct helper;
+template <int N, int I> struct helper<N, I, array_layout::ROW_MAJOR> {
+  template <typename T, typename F, typename... TupleElementTypes> static OVK_FORCE_INLINE void
+    ForEach(const interval<T,N> &Interval, F &&Func, TupleElementTypes... TupleElements) {
+    for (T i = Interval.Begin(N-1-I); i < Interval.End(N-1-I); ++i) {
+      helper<N, I-1, array_layout::ROW_MAJOR>::ForEach(Interval, std::forward<F>(Func),
+        TupleElements..., i);
+    }
+  }
+};
+template <int N, int I> struct helper<N, I, array_layout::COLUMN_MAJOR> {
+  template <typename T, typename F, typename... TupleElementTypes> static OVK_FORCE_INLINE void
+    ForEach(const interval<T,N> &Interval, F &&Func, TupleElementTypes... TupleElements) {
+    for (T i = Interval.Begin(I); i < Interval.End(I); ++i) {
+      helper<N, I-1, array_layout::COLUMN_MAJOR>::ForEach(Interval, std::forward<F>(Func),
+        i, TupleElements...);
+    }
+  }
+};
+template <int N> struct helper<N, 0, array_layout::ROW_MAJOR> {
+  template <typename T, typename F, typename... TupleElementTypes, OVK_FUNCTION_REQUIRES(
+    IsCallableWithTupleElements<F &&, T, N>())> static OVK_FORCE_INLINE void ForEach(const
+    interval<T,N> &Interval, F &&Func, TupleElementTypes... TupleElements) {
+    for (T i = Interval.Begin(N-1); i < Interval.End(N-1); ++i) {
+      Func(TupleElements...,i);
+    }
+  }
+  template <typename T, typename F, typename... TupleElementTypes, OVK_FUNCTION_REQUIRES(
+    !IsCallableWithTupleElements<F &&, T, N>() && IsCallableWith<F &&, const elem<T,N> &>())>
+    static OVK_FORCE_INLINE void ForEach(const interval<T,N> &Interval, F &&Func,
+    TupleElementTypes... TupleElements) {
+    for (T i = Interval.Begin(N-1); i < Interval.End(N-1); ++i) {
+      Func(elem<T,N>(TupleElements...,i));
+    }
+  }
+};
+template <int N> struct helper<N, 0, array_layout::COLUMN_MAJOR> {
+  template <typename T, typename F, typename... TupleElementTypes, OVK_FUNCTION_REQUIRES(
+    IsCallableWithTupleElements<F &&, T, N>())> static OVK_FORCE_INLINE void ForEach(const
+    interval<T,N> &Interval, F &&Func, TupleElementTypes... TupleElements) {
+    for (T i = Interval.Begin(0); i < Interval.End(0); ++i) {
+      Func(i,TupleElements...);
+    }
+  }
+  template <typename T, typename F, typename... TupleElementTypes, OVK_FUNCTION_REQUIRES(
+    !IsCallableWithTupleElements<F &&, T, N>() && IsCallableWith<F &&, const elem<T,N> &>())>
+    static OVK_FORCE_INLINE void ForEach(const interval<T,N> &Interval, F &&Func,
+    TupleElementTypes... TupleElements) {
+    for (T i = Interval.Begin(0); i < Interval.End(0); ++i) {
+      Func(elem<T,N>(i,TupleElements...));
+    }
+  }
+};
+
+}
+
+template <array_layout Layout=array_layout::ROW_MAJOR, typename T, int N, typename F,
+  OVK_FUNCTION_REQUIRES(IsCallableWith<F &&, const elem<T,N> &>() || for_each_internal::
+  IsCallableWithTupleElements<F &&, T, N>())> OVK_FORCE_INLINE void ForEach(const interval<T,N>
+  &Interval, F &&Func) {
+  // Have to use N-1 and count down because we can specialize on (N, 0) but not (N, N-1)
+  return for_each_internal::helper<N, N-1, Layout>::ForEach(Interval, std::forward<F>(Func));
+}
+
+}}
+
+#endif
---
+++ tests/ForEachTests.cpp
@@ -0,0 +1,118 @@
+// Copyright (c) 2018 Matthew J. Smith and Overkit contributors
+// License: MIT (http://opensource.org/licenses/MIT)
+
+#include <ovk/core/ForEach.hpp>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "MPITest.hpp"
+
+#include <ovk/core/Comm.hpp>
+#include <ovk/core/Elem.hpp>
+#include <ovk/core/Interval.hpp>
+#include <ovk/core/Vector.hpp>
+
+#include <mpi.h>
+
+using testing::ElementsAre;
+
+class ForEachTests : public tests::mpi_test {};
+
+TEST_F(ForEachTests, ForEachTupleInInterval) {
+
+  if (TestComm().Rank() != 0) return;
+
+  // One-dimensional, tuple
+  {
+    ovk::interval<int> Interval(1, 5);
+    ovk::core::vector<ovk::elem<int>> Tuples;
+    ovk::core::ForEach(Interval, [&](const ovk::elem<int> &Tuple) {
+      Tuples.Append(Tuple);
+    });
+    EXPECT_EQ(Tuples.Count(), 4);
+    EXPECT_THAT(Tuples[0], ElementsAre(1));
+    EXPECT_THAT(Tuples[1], ElementsAre(2));
+    EXPECT_THAT(Tuples[2], ElementsAre(3));
+    EXPECT_THAT(Tuples[3], ElementsAre(4));
+  }
+
+  // One-dimensional, tuple element
+  {
+    ovk::interval<int> Interval(1, 5);
+    ovk::core::vector<ovk::elem<int>> Tuples;
+    ovk::core::ForEach(Interval, [&](int i) {
+      Tuples.Append(ovk::elem<int>(i));
+    });
+    EXPECT_EQ(Tuples.Count(), 4);
+    EXPECT_THAT(Tuples[0], ElementsAre(1));
+    EXPECT_THAT(Tuples[1], ElementsAre(2));
+    EXPECT_THAT(Tuples[2], ElementsAre(3));
+    EXPECT_THAT(Tuples[3], ElementsAre(4));
+  }
+
+  // Multidimensional, row major, tuple
+  {
+    ovk::interval<int,3> Interval({1,2,3}, {2,4,6});
+    ovk::core::vector<ovk::elem<int,3>> Tuples;
+    ovk::core::ForEach<ovk::array_layout::ROW_MAJOR>(Interval, [&](const ovk::elem<int,3> &Tuple) {
+      Tuples.Append(Tuple);
+    });
+    EXPECT_EQ(Tuples.Count(), 6);
+    EXPECT_THAT(Tuples[0], ElementsAre(1,2,3));
+    EXPECT_THAT(Tuples[1], ElementsAre(1,2,4));
+    EXPECT_THAT(Tuples[2], ElementsAre(1,2,5));
+    EXPECT_THAT(Tuples[3], ElementsAre(1,3,3));
+    EXPECT_THAT(Tuples[4], ElementsAre(1,3,4));
+    EXPECT_THAT(Tuples[5], ElementsAre(1,3,5));
+  }
+
+  // Multidimensional, row major, tuple elements
+  {
+    ovk::interval<int,3> Interval({1,2,3}, {2,4,6});
+    ovk::core::vector<ovk::elem<int,3>> Tuples;
+    ovk::core::ForEach<ovk::array_layout::ROW_MAJOR>(Interval, [&](int i, int j, int k) {
+      Tuples.Append(ovk::elem<int,3>(i,j,k));
+    });
+    EXPECT_EQ(Tuples.Count(), 6);
+    EXPECT_THAT(Tuples[0], ElementsAre(1,2,3));
+    EXPECT_THAT(Tuples[1], ElementsAre(1,2,4));
+    EXPECT_THAT(Tuples[2], ElementsAre(1,2,5));
+    EXPECT_THAT(Tuples[3], ElementsAre(1,3,3));
+    EXPECT_THAT(Tuples[4], ElementsAre(1,3,4));
+    EXPECT_THAT(Tuples[5], ElementsAre(1,3,5));
+  }
+
+  // Multidimensional, column major, tuple
+  {
+    ovk::interval<int,3> Interval({1,2,3}, {2,4,6});
+    ovk::core::vector<ovk::elem<int,3>> Tuples;
+    ovk::core::ForEach<ovk::array_layout::COLUMN_MAJOR>(Interval, [&](const ovk::elem<int,3>
+      &Tuple) {
+      Tuples.Append(Tuple);
+    });
+    EXPECT_EQ(Tuples.Count(), 6);
+    EXPECT_THAT(Tuples[0], ElementsAre(1,2,3));
+    EXPECT_THAT(Tuples[1], ElementsAre(1,3,3));
+    EXPECT_THAT(Tuples[2], ElementsAre(1,2,4));
+    EXPECT_THAT(Tuples[3], ElementsAre(1,3,4));
+    EXPECT_THAT(Tuples[4], ElementsAre(1,2,5));
+    EXPECT_THAT(Tuples[5], ElementsAre(1,3,5));
+  }
+
+  // Multidimensional, column major, tuple elements
+  {
+    ovk::interval<int,3> Interval({1,2,3}, {2,4,6});
+    ovk::core::vector<ovk::elem<int,3>> Tuples;
+    ovk::core::ForEach<ovk::array_layout::COLUMN_MAJOR>(Interval, [&](int i, int j, int k) {
+      Tuples.Append(ovk::elem<int,3>(i,j,k));
+    });
+    EXPECT_EQ(Tuples.Count(), 6);
+    EXPECT_THAT(Tuples[0], ElementsAre(1,2,3));
+    EXPECT_THAT(Tuples[1], ElementsAre(1,3,3));
+    EXPECT_THAT(Tuples[2], ElementsAre(1,2,4));
+    EXPECT_THAT(Tuples[3], ElementsAre(1,3,4));
+    EXPECT_THAT(Tuples[4], ElementsAre(1,2,5));
+    EXPECT_THAT(Tuples[5], ElementsAre(1,3,5));
+  }
+
+}
