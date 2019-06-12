12cce0a03b90fd999db07bec62dcee7bc81e2242
Matthew Smith <mjsmith6@illinois.edu>
2019-06-12 11:35:42 -0500

    added id_set type for storing set of IDs (or ID tuples)

---
+++ src/core/IDSet.hpp
@@ -0,0 +1,307 @@
+// Copyright (c) 2018 Matthew J. Smith and Overkit contributors
+// License: MIT (http://opensource.org/licenses/MIT)
+
+#ifndef OVK_CORE_ID_SET_HPP_INCLUDED
+#define OVK_CORE_ID_SET_HPP_INCLUDED
+
+#include <ovk/core/Array.hpp>
+#include <ovk/core/Elem.hpp>
+#include <ovk/core/Global.hpp>
+#include <ovk/core/IteratorTraits.hpp>
+#include <ovk/core/Requires.hpp>
+
+#include <algorithm>
+#include <initializer_list>
+#include <iterator>
+#include <memory>
+#include <utility>
+
+namespace ovk {
+
+namespace id_set_internal {
+
+template <int Rank, typename IDTypeSequence> class id_set_base;
+
+template <int Rank, typename... IDTypes> class id_set_base<Rank, core::type_sequence<IDTypes...>> {
+
+public:
+
+  using value_type = elem<int,Rank>;
+  // Values aren't mutable, so iterator is const
+  using iterator = const value_type *;
+  using const_iterator = iterator;
+
+  void Insert(IDTypes... IDs) {
+    value_type Value(IDs...);
+    auto ValuesIter = LowerBound_(Value);
+    if (ValuesIter == Values_.LinearEnd() || Less(Value, *ValuesIter)) {
+      ValuesIter = Values_.Insert(ValuesIter, Value);
+    }
+  }
+
+  void Erase(IDTypes... IDs) {
+    value_type Value(IDs...);
+    auto ValuesIter = LowerBound_(Value);
+    if (ValuesIter != Values_.LinearEnd() && !Less(Value, *ValuesIter)) {
+      Values_.Erase(ValuesIter);
+    }
+  }
+
+  bool Contains(IDTypes... IDs) const {
+    value_type Value(IDs...);
+    auto ValuesIter = LowerBound_(Value);
+    return ValuesIter != Values_.LinearEnd() && !Less(Value, *ValuesIter);
+  }
+
+  iterator Find(IDTypes... IDs) const {
+    value_type Value(IDs...);
+    auto ValuesIter = LowerBound_(Value);
+    if (ValuesIter != Values_.LinearEnd() && Less(Value, *ValuesIter)) {
+      ValuesIter = Values_.LinearEnd();
+    }
+    return Values_.Data() + (ValuesIter - Values_.LinearBegin());
+  }
+
+  iterator LowerBound(IDTypes... IDs) const {
+    value_type Value(IDs...);
+    auto ValuesIter = LowerBound_(Value);
+    return Values_.Data() + (ValuesIter - Values_.LinearBegin());
+  }
+
+  iterator UpperBound(IDTypes... IDs) const {
+    value_type Value(IDs...);
+    auto ValuesIter = UpperBound_(Value);
+    return Values_.Data() + (ValuesIter - Values_.LinearBegin());
+  }
+
+  static constexpr bool Less(const value_type &Left, const value_type &Right) {
+    return Less_(Left, Right, index_tag<0>());
+  }
+
+protected:
+
+  template <int N> struct index_tag {};
+
+  array<value_type> Values_;
+
+  typename array<value_type>::const_iterator LowerBound_(const value_type &Value) const {
+    auto Compare = [](const value_type &Left, const value_type &Right) -> bool {
+      return Less(Left, Right);
+    };
+    return std::lower_bound(Values_.LinearBegin(), Values_.LinearEnd(), Value, Compare);
+  }
+
+  typename array<value_type>::iterator LowerBound_(const value_type &Value) {
+    auto Compare = [](const value_type &Left, const value_type &Right) -> bool {
+      return Less(Left, Right);
+    };
+    return std::lower_bound(Values_.LinearBegin(), Values_.LinearEnd(), Value, Compare);
+  }
+
+  typename array<value_type>::const_iterator UpperBound_(const value_type &Value) const {
+    auto Compare = [](const value_type &Left, const value_type &Right) -> bool {
+      return Less(Left, Right);
+    };
+    return std::upper_bound(Values_.LinearBegin(), Values_.LinearEnd(), Value, Compare);
+  }
+
+  typename array<value_type>::iterator UpperBound_(const value_type &Value) {
+    auto Compare = [](const value_type &Left, const value_type &Right) -> bool {
+      return Less(Left, Right);
+    };
+    return std::upper_bound(Values_.LinearBegin(), Values_.LinearEnd(), Value, Compare);
+  }
+
+  template <int N> static constexpr OVK_FORCE_INLINE bool Less_(const value_type &Left, const
+    value_type &Right, index_tag<N>) {
+    return Left(N) < Right(N) || (Left(N) == Right(N) && Less_(Left, Right, index_tag<N+1>()));
+  }
+
+  static constexpr OVK_FORCE_INLINE bool Less_(const value_type &Left, const value_type &Right,
+    index_tag<Rank-1>) {
+    return Left(Rank-1) < Right(Rank-1);
+  }
+
+};
+
+}
+
+template <int Rank_> class id_set : public id_set_internal::id_set_base<Rank_,
+  core::repeated_type_sequence_of_size<int, Rank_>> {
+
+private:
+
+  using parent_type = id_set_internal::id_set_base<Rank_, core::repeated_type_sequence_of_size<int,
+    Rank_>>;
+
+  using parent_type::Values_;
+  using parent_type::LowerBound_;
+  using parent_type::UpperBound_;
+
+public:
+
+  static constexpr int Rank = Rank_;
+  using value_type = typename parent_type::value_type;
+  using iterator = typename parent_type::iterator;
+  using const_iterator = typename parent_type::const_iterator;
+
+  using parent_type::Insert;
+  using parent_type::Erase;
+  using parent_type::Contains;
+  using parent_type::Find;
+  using parent_type::LowerBound;
+  using parent_type::UpperBound;
+  using parent_type::Less;
+
+  id_set() = default;
+
+  id_set(std::initializer_list<value_type> ValuesList) {
+    Values_.Reserve(ValuesList.size());
+    for (auto &Value : ValuesList) {
+      Insert(Value);
+    }
+  }
+
+  // Don't care about input iterators enough to implement a second overload
+  template <typename IterType, OVK_FUNCTION_REQUIRES(core::IsForwardIterator<IterType>())>
+    id_set(IterType Begin, IterType End) {
+    Values_.Reserve(std::distance(Begin, End));
+    IterType Iter = Begin;
+    while (Iter != End) {
+      Insert(*Iter);
+      ++Iter;
+    }
+  }
+
+  id_set &operator=(std::initializer_list<value_type> ValuesList) {
+    return Assign(ValuesList);
+  }
+
+  id_set &Assign(std::initializer_list<value_type> ValuesList) {
+    Values_.Clear();
+    Values_.Reserve(ValuesList.size());
+    for (auto &Value : ValuesList) {
+      Insert(Value);
+    }
+    return *this;
+  }
+
+  // Don't care about input iterators enough to implement a second overload
+  template <typename IterType, OVK_FUNCTION_REQUIRES(core::IsForwardIterator<IterType>())>
+    id_set &Assign(IterType Begin, IterType End) {
+    Values_.Clear();
+    Values_.Reserve(std::distance(Begin, End));
+    IterType Iter = Begin;
+    while (Iter != End) {
+      Insert(*Iter);
+      ++Iter;
+    }
+    return *this;
+  }
+
+  void Reserve(long long Count) {
+    Values_.Reserve(Count);
+  }
+
+  void Insert(const value_type &Value) {
+    auto ValuesIter = LowerBound_(Value);
+    if (ValuesIter == Values_.LinearEnd() || Less(Value, *ValuesIter)) {
+      ValuesIter = Values_.Insert(ValuesIter, Value);
+    }
+  }
+
+  void Insert(iterator LowerBoundIter, const value_type &Value) {
+    auto ValuesIter = Values_.LinearBegin() + (LowerBoundIter - Values_.Data());
+    if (ValuesIter == Values_.LinearEnd() || Less(Value, *ValuesIter)) {
+      ValuesIter = Values_.Insert(ValuesIter, Value);
+    }
+  }
+
+  void Erase(const value_type &Value) {
+    auto ValuesIter = LowerBound_(Value);
+    if (ValuesIter != Values_.LinearEnd() && !Less(Value, *ValuesIter)) {
+      Values_.Erase(ValuesIter);
+    }
+  }
+
+  iterator Erase(iterator Pos) {
+    auto ValuesIter = Values_.LinearBegin() + (Pos - Values_.Data());
+    ValuesIter = Values_.Erase(ValuesIter);
+    return Values_.Data(ValuesIter - Values_.LinearBegin());
+  }
+
+  void Clear() {
+    Values_.Clear();
+  }
+
+  bool Contains(const value_type &Value) const {
+    auto ValuesIter = LowerBound_(Value);
+    return ValuesIter != Values_.LinearEnd() && !Less(Value, *ValuesIter);
+  }
+
+  iterator Find(const value_type &Value) const {
+    auto ValuesIter = LowerBound_(Value);
+    if (ValuesIter != Values_.LinearEnd() && Less(Value, *ValuesIter)) {
+      ValuesIter = Values_.LinearEnd();
+    }
+    return Values_.Data() + (ValuesIter - Values_.LinearBegin());
+  }
+
+  iterator LowerBound(const value_type &Value) const {
+    auto ValuesIter = LowerBound_(Value);
+    return Values_.Data() + (ValuesIter - Values_.LinearBegin());
+  }
+
+  iterator UpperBound(const value_type &Value) const {
+    auto ValuesIter = UpperBound_(Value);
+    return Values_.Data() + (ValuesIter - Values_.LinearBegin());
+  }
+
+  // Not sure if this is actually useful for Rank > 1
+  value_type NextAvailableValue(int iDim=0) const {
+    value_type Result = MakeUniformElem<int,Rank>(0);
+    if (Values_.Count() > 0 && Values_(0)(iDim) == 0) {
+      int PrevID = Values_(0)(iDim);
+      for (int iValue = 1; iValue < Values_.Count(); ++iValue) {
+        int ID = Values_(iValue)(iDim);
+        if (ID - PrevID > 1) {
+          break;
+        }
+        PrevID = ID;
+      }
+      Result(iDim) = PrevID + 1;
+    }
+    return Result;
+  }
+
+  long long Count() const { return Values_.Count(); }
+
+  bool Empty() const { return Values_.Empty(); }
+
+  long long Capacity() const { return Values_.Capacity(); }
+
+  const value_type &operator[](long long Index) const { return Values_(Index); }
+
+  const value_type *Data() const { return Values_.Data(); }
+
+  iterator Begin() const { return Values_.Data(); }
+
+  iterator End() const { return Values_.Data() + Values_.Count(); }
+
+private:
+
+  friend class core::test_helper<id_set>;
+
+};
+
+template <int Rank> typename id_set<Rank>::iterator begin(const id_set<Rank> &Set) {
+  return Set.Begin();
+}
+
+template <int Rank> typename id_set<Rank>::iterator end(const id_set<Rank> &Set) {
+  return Set.End();
+}
+
+}
+
+#endif
---
+++ tests/IDSetTests.cpp
@@ -0,0 +1,649 @@
+// Copyright (c) 2018 Matthew J. Smith and Overkit contributors
+// License: MIT (http://opensource.org/licenses/MIT)
+
+#include <ovk/core/IDSet.hpp>
+
+#include "tests/MPITest.hpp"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include <ovk/core/Elem.hpp>
+
+#include <array>
+#include <mpi.h>
+
+#include <utility>
+
+class IDSetTests : public tests::mpi_test {};
+
+using testing::ElementsAre;
+
+namespace ovk {
+namespace core {
+template <int Rank> class test_helper<id_set<Rank>> {
+public:
+  using id_set_type = id_set<Rank>;
+  using value_type = typename id_set<Rank>::value_type;
+  static const array<value_type> &GetValues(const id_set_type &IDSet) { return IDSet.Values_; }
+  static array<value_type> &GetValues(id_set_type &IDSet) { return IDSet.Values_; }
+};
+}}
+
+TEST_F(IDSetTests, Meta) {
+
+  if (TestComm().Rank() != 0) return;
+
+  using id_set = ovk::id_set<2>;
+
+  EXPECT_EQ(int(id_set::Rank), 2);
+  EXPECT_TRUE((std::is_same<typename id_set::value_type, ovk::elem<int,2>>::value));
+  EXPECT_TRUE((std::is_same<typename id_set::iterator, const ovk::elem<int,2> *>::value));
+  EXPECT_TRUE((std::is_same<typename id_set::const_iterator, const ovk::elem<int,2> *>::value));
+
+}
+
+TEST_F(IDSetTests, Create) {
+
+  if (TestComm().Rank() != 0) return;
+
+  using id_set = ovk::id_set<2>;
+  using helper = ovk::core::test_helper<id_set>;
+
+  // Default
+  {
+    id_set IDSet;
+    auto &Values = helper::GetValues(IDSet);
+    EXPECT_EQ(Values.Count(), 0);
+  }
+
+  // Initializer list
+  {
+    id_set IDSet = {{1,2}, {1,3}};
+    auto &Values = helper::GetValues(IDSet);
+    EXPECT_EQ(Values.Count(), 2);
+    EXPECT_THAT(Values(0), ElementsAre(1,2));
+    EXPECT_THAT(Values(1), ElementsAre(1,3));
+  }
+
+  // Iterators
+  {
+    std::array<ovk::elem<int,2>,2> SourceValues = {{{1,2}, {1,3}}};
+    id_set IDSet(SourceValues.begin(), SourceValues.end());
+    auto &Values = helper::GetValues(IDSet);
+    EXPECT_EQ(Values.Count(), 2);
+    EXPECT_THAT(Values(0), ElementsAre(1,2));
+    EXPECT_THAT(Values(1), ElementsAre(1,3));
+  }
+
+}
+
+TEST_F(IDSetTests, Copy) {
+
+  if (TestComm().Rank() != 0) return;
+
+  using id_set = ovk::id_set<2>;
+  using helper = ovk::core::test_helper<id_set>;
+
+  // Copy construct
+  {
+    id_set IDSet1 = {{1,2}, {1,3}};
+    id_set IDSet2 = IDSet1;
+    auto &Values1 = helper::GetValues(IDSet1);
+    auto &Values2 = helper::GetValues(IDSet2);
+    EXPECT_EQ(Values1.Count(), 2);
+    EXPECT_THAT(Values1(0), ElementsAre(1,2));
+    EXPECT_THAT(Values1(1), ElementsAre(1,3));
+    EXPECT_EQ(Values2.Count(), 2);
+    EXPECT_THAT(Values2(0), ElementsAre(1,2));
+    EXPECT_THAT(Values2(1), ElementsAre(1,3));
+  }
+
+  // Copy assign
+  {
+    id_set IDSet1 = {{1,2}, {1,3}};
+    id_set IDSet2;
+    IDSet2 = IDSet1;
+    auto &Values1 = helper::GetValues(IDSet1);
+    auto &Values2 = helper::GetValues(IDSet2);
+    EXPECT_EQ(Values1.Count(), 2);
+    EXPECT_THAT(Values1(0), ElementsAre(1,2));
+    EXPECT_THAT(Values1(1), ElementsAre(1,3));
+    EXPECT_EQ(Values2.Count(), 2);
+    EXPECT_THAT(Values2(0), ElementsAre(1,2));
+    EXPECT_THAT(Values2(1), ElementsAre(1,3));
+  }
+
+}
+
+TEST_F(IDSetTests, Move) {
+
+  if (TestComm().Rank() != 0) return;
+
+  using id_set = ovk::id_set<2>;
+  using helper = ovk::core::test_helper<id_set>;
+
+  // Move construct
+  {
+    id_set IDSet1 = {{1,2}, {1,3}};
+    id_set IDSet2(std::move(IDSet1));
+    auto &Values1 = helper::GetValues(IDSet1);
+    auto &Values2 = helper::GetValues(IDSet2);
+    EXPECT_EQ(Values1.Count(), 0);
+    EXPECT_EQ(Values2.Count(), 2);
+    EXPECT_THAT(Values2(0), ElementsAre(1,2));
+    EXPECT_THAT(Values2(1), ElementsAre(1,3));
+  }
+
+  // Move assign
+  {
+    id_set IDSet1 = {{1,2}, {1,3}};
+    id_set IDSet2;
+    IDSet2 = std::move(IDSet1);
+    auto &Values1 = helper::GetValues(IDSet1);
+    auto &Values2 = helper::GetValues(IDSet2);
+    EXPECT_EQ(Values1.Count(), 0);
+    EXPECT_EQ(Values2.Count(), 2);
+    EXPECT_THAT(Values2(0), ElementsAre(1,2));
+    EXPECT_THAT(Values2(1), ElementsAre(1,3));
+  }
+
+}
+
+TEST_F(IDSetTests, Assign) {
+
+  if (TestComm().Rank() != 0) return;
+
+  using id_set = ovk::id_set<2>;
+  using helper = ovk::core::test_helper<id_set>;
+
+  // Initializer list, operator=
+  {
+    id_set IDSet = {{0,1}, {0,2}};
+    IDSet = {{1,2}, {1,3}};
+    auto &Values = helper::GetValues(IDSet);
+    EXPECT_EQ(Values.Count(), 2);
+    EXPECT_THAT(Values(0), ElementsAre(1,2));
+    EXPECT_THAT(Values(1), ElementsAre(1,3));
+  }
+
+  // Initializer list, Assign
+  {
+    id_set IDSet = {{0,1}, {0,2}};
+    IDSet.Assign({{1,2}, {1,3}});
+    auto &Values = helper::GetValues(IDSet);
+    EXPECT_EQ(Values.Count(), 2);
+    EXPECT_THAT(Values(0), ElementsAre(1,2));
+    EXPECT_THAT(Values(1), ElementsAre(1,3));
+  }
+
+  // Iterators, Assign
+  {
+    id_set IDSet = {{0,1}, {0,2}};
+    std::array<ovk::elem<int,2>,2> SourceValues = {{{1,2}, {1,3}}};
+    IDSet.Assign(SourceValues.begin(), SourceValues.end());
+    auto &Values = helper::GetValues(IDSet);
+    EXPECT_EQ(Values.Count(), 2);
+    EXPECT_THAT(Values(0), ElementsAre(1,2));
+    EXPECT_THAT(Values(1), ElementsAre(1,3));
+  }
+
+}
+
+TEST_F(IDSetTests, Reserve) {
+
+  if (TestComm().Rank() != 0) return;
+
+  using id_set = ovk::id_set<2>;
+  using helper = ovk::core::test_helper<id_set>;
+
+  id_set IDSet;
+  auto &Values = helper::GetValues(IDSet);
+  long long Capacity = Values.Capacity();
+  IDSet.Reserve(Capacity+1);
+  EXPECT_GE(Values.Capacity(), Capacity+1);
+
+}
+
+TEST_F(IDSetTests, Insert) {
+
+  if (TestComm().Rank() != 0) return;
+
+  using id_set = ovk::id_set<2>;
+  using helper = ovk::core::test_helper<id_set>;
+
+  // Value, doesn't already exist
+  {
+    id_set IDSet = {{1,2}, {2,1}};
+    IDSet.Insert({1,3});
+    auto &Values = helper::GetValues(IDSet);
+    EXPECT_EQ(Values.Count(), 3);
+    EXPECT_THAT(Values(0), ElementsAre(1,2));
+    EXPECT_THAT(Values(1), ElementsAre(1,3));
+    EXPECT_THAT(Values(2), ElementsAre(2,1));
+  }
+
+  // Value, already exists
+  {
+    id_set IDSet = {{1,2}, {2,1}};
+    IDSet.Insert({2,1});
+    auto &Values = helper::GetValues(IDSet);
+    EXPECT_EQ(Values.Count(), 2);
+    EXPECT_THAT(Values(0), ElementsAre(1,2));
+    EXPECT_THAT(Values(1), ElementsAre(2,1));
+  }
+
+  // Separate IDs, doesn't already exist
+  {
+    id_set IDSet = {{1,2}, {2,1}};
+    IDSet.Insert(1,3);
+    auto &Values = helper::GetValues(IDSet);
+    EXPECT_EQ(Values.Count(), 3);
+    EXPECT_THAT(Values(0), ElementsAre(1,2));
+    EXPECT_THAT(Values(1), ElementsAre(1,3));
+    EXPECT_THAT(Values(2), ElementsAre(2,1));
+  }
+
+  // Separate IDs, already exists
+  {
+    id_set IDSet = {{1,2}, {2,1}};
+    IDSet.Insert(2,1);
+    auto &Values = helper::GetValues(IDSet);
+    EXPECT_EQ(Values.Count(), 2);
+    EXPECT_THAT(Values(0), ElementsAre(1,2));
+    EXPECT_THAT(Values(1), ElementsAre(2,1));
+  }
+
+  // With lower bound iterator, doesn't already exist
+  {
+    id_set IDSet = {{1,2}, {2,1}};
+    auto &Values = helper::GetValues(IDSet);
+    IDSet.Insert(IDSet.Begin()+1, {1,3});
+    EXPECT_EQ(Values.Count(), 3);
+    EXPECT_THAT(Values(0), ElementsAre(1,2));
+    EXPECT_THAT(Values(1), ElementsAre(1,3));
+    EXPECT_THAT(Values(2), ElementsAre(2,1));
+  }
+
+  // With lower bound iterator, already exists
+  {
+    id_set IDSet = {{1,2}, {2,1}};
+    auto &Values = helper::GetValues(IDSet);
+    IDSet.Insert(IDSet.Begin()+1, {2,1});
+    EXPECT_EQ(Values.Count(), 2);
+    EXPECT_THAT(Values(0), ElementsAre(1,2));
+    EXPECT_THAT(Values(1), ElementsAre(2,1));
+  }
+
+}
+
+TEST_F(IDSetTests, Erase) {
+
+  if (TestComm().Rank() != 0) return;
+
+  using id_set = ovk::id_set<2>;
+  using helper = ovk::core::test_helper<id_set>;
+
+  // Value, exists
+  {
+    id_set IDSet = {{1,2}, {1,3}, {2,1}};
+    IDSet.Erase({1,3});
+    auto &Values = helper::GetValues(IDSet);
+    EXPECT_EQ(Values.Count(), 2);
+    EXPECT_THAT(Values(0), ElementsAre(1,2));
+    EXPECT_THAT(Values(1), ElementsAre(2,1));
+  }
+
+  // Value, doesn't exist
+  {
+    id_set IDSet = {{1,2}, {2,1}};
+    IDSet.Erase({1,3});
+    auto &Values = helper::GetValues(IDSet);
+    EXPECT_EQ(Values.Count(), 2);
+    EXPECT_THAT(Values(0), ElementsAre(1,2));
+    EXPECT_THAT(Values(1), ElementsAre(2,1));
+  }
+
+  // Separate IDs, exists
+  {
+    id_set IDSet = {{1,2}, {1,3}, {2,1}};
+    IDSet.Erase(1,3);
+    auto &Values = helper::GetValues(IDSet);
+    EXPECT_EQ(Values.Count(), 2);
+    EXPECT_THAT(Values(0), ElementsAre(1,2));
+    EXPECT_THAT(Values(1), ElementsAre(2,1));
+  }
+
+  // Separate IDs, doesn't exist
+  {
+    id_set IDSet = {{1,2}, {2,1}};
+    IDSet.Erase(1,3);
+    auto &Values = helper::GetValues(IDSet);
+    EXPECT_EQ(Values.Count(), 2);
+    EXPECT_THAT(Values(0), ElementsAre(1,2));
+    EXPECT_THAT(Values(1), ElementsAre(2,1));
+  }
+
+  // Iterator
+  {
+    id_set IDSet = {{1,2}, {1,3}, {2,1}};
+    IDSet.Erase(IDSet.Begin()+1);
+    auto &Values = helper::GetValues(IDSet);
+    EXPECT_EQ(Values.Count(), 2);
+    EXPECT_THAT(Values(0), ElementsAre(1,2));
+    EXPECT_THAT(Values(1), ElementsAre(2,1));
+  }
+
+}
+
+TEST_F(IDSetTests, Clear) {
+
+  if (TestComm().Rank() != 0) return;
+
+  using id_set = ovk::id_set<2>;
+  using helper = ovk::core::test_helper<id_set>;
+
+  // Empty
+  {
+    id_set IDSet;
+    IDSet.Clear();
+    auto &Values = helper::GetValues(IDSet);
+    EXPECT_TRUE(Values.Empty());
+  }
+
+  // Non-empty
+  {
+    id_set IDSet = {{1,2}, {1,3}};
+    IDSet.Clear();
+    auto &Values = helper::GetValues(IDSet);
+    EXPECT_TRUE(Values.Empty());
+  }
+
+}
+
+TEST_F(IDSetTests, Contains) {
+
+  if (TestComm().Rank() != 0) return;
+
+  using id_set = ovk::id_set<2>;
+
+  // Value, exists
+  {
+    id_set IDSet = {{1,2}, {1,3}, {2,1}};
+    EXPECT_TRUE(IDSet.Contains({1,3}));
+  }
+
+  // Value, doesn't exist
+  {
+    id_set IDSet = {{1,2}, {2,1}};
+    EXPECT_FALSE(IDSet.Contains({1,3}));
+  }
+
+  // Separate IDs, exists
+  {
+    id_set IDSet = {{1,2}, {1,3}, {2,1}};
+    EXPECT_TRUE(IDSet.Contains(1,3));
+  }
+
+  // Separate IDs, doesn't exist
+  {
+    id_set IDSet = {{1,2}, {2,1}};
+    EXPECT_FALSE(IDSet.Contains(1,3));
+  }
+
+}
+
+TEST_F(IDSetTests, Find) {
+
+  if (TestComm().Rank() != 0) return;
+
+  using id_set = ovk::id_set<2>;
+
+  // Value, exists
+  {
+    id_set IDSet = {{1,2}, {1,3}, {2,1}};
+    auto Iter = IDSet.Find({1,3});
+    EXPECT_EQ(Iter, IDSet.Begin()+1);
+  }
+
+  // Value, doesn't exist
+  {
+    id_set IDSet = {{1,2}, {2,1}};
+    auto Iter = IDSet.Find({1,3});
+    EXPECT_EQ(Iter, IDSet.End());
+  }
+
+  // Separate IDs, exists
+  {
+    id_set IDSet = {{1,2}, {1,3}, {2,1}};
+    auto Iter = IDSet.Find(1,3);
+    EXPECT_EQ(Iter, IDSet.Begin()+1);
+  }
+
+  // Separate IDs, doesn't exist
+  {
+    id_set IDSet = {{1,2}, {2,1}};
+    auto Iter = IDSet.Find(1,3);
+    EXPECT_EQ(Iter, IDSet.End());
+  }
+
+}
+
+TEST_F(IDSetTests, LowerBound) {
+
+  if (TestComm().Rank() != 0) return;
+
+  using id_set = ovk::id_set<2>;
+
+  // Value, exists
+  {
+    id_set IDSet = {{1,2}, {1,3}, {2,1}};
+    auto Iter = IDSet.LowerBound({1,3});
+    EXPECT_EQ(Iter, IDSet.Begin()+1);
+  }
+
+  // Value, doesn't exist
+  {
+    id_set IDSet = {{1,2}, {2,1}};
+    auto Iter = IDSet.LowerBound({1,3});
+    EXPECT_EQ(Iter, IDSet.Begin()+1);
+  }
+
+  // Separate IDs, exists
+  {
+    id_set IDSet = {{1,2}, {1,3}, {2,1}};
+    auto Iter = IDSet.LowerBound(1,3);
+    EXPECT_EQ(Iter, IDSet.Begin()+1);
+  }
+
+  // Separate IDs, doesn't exist
+  {
+    id_set IDSet = {{1,2}, {2,1}};
+    auto Iter = IDSet.LowerBound(1,3);
+    EXPECT_EQ(Iter, IDSet.Begin()+1);
+  }
+
+}
+
+TEST_F(IDSetTests, UpperBound) {
+
+  if (TestComm().Rank() != 0) return;
+
+  using id_set = ovk::id_set<2>;
+
+  // Value, exists
+  {
+    id_set IDSet = {{1,2}, {1,3}, {2,1}};
+    auto Iter = IDSet.UpperBound({1,3});
+    EXPECT_EQ(Iter, IDSet.Begin()+2);
+  }
+
+  // Value, doesn't exist
+  {
+    id_set IDSet = {{1,2}, {2,1}};
+    auto Iter = IDSet.UpperBound({1,3});
+    EXPECT_EQ(Iter, IDSet.Begin()+1);
+  }
+
+  // Separate IDs, exists
+  {
+    id_set IDSet = {{1,2}, {1,3}, {2,1}};
+    auto Iter = IDSet.UpperBound(1,3);
+    EXPECT_EQ(Iter, IDSet.Begin()+2);
+  }
+
+  // Separate IDs, doesn't exist
+  {
+    id_set IDSet = {{1,2}, {2,1}};
+    auto Iter = IDSet.UpperBound(1,3);
+    EXPECT_EQ(Iter, IDSet.Begin()+1);
+  }
+
+}
+
+TEST_F(IDSetTests, NextAvailableValue) {
+
+  if (TestComm().Rank() != 0) return;
+
+  using id_set = ovk::id_set<1>;
+
+  // First ID > 0
+  {
+    id_set IDSet = {1, 2};
+    EXPECT_EQ(IDSet.NextAvailableValue(), 0);
+  }
+
+  // Gap
+  {
+    id_set IDSet = {0, 2};
+    EXPECT_EQ(IDSet.NextAvailableValue(), 1);
+  }
+
+  // End
+  {
+    id_set IDSet = {0, 1};
+    EXPECT_EQ(IDSet.NextAvailableValue(), 2);
+  }
+
+}
+
+TEST_F(IDSetTests, Count) {
+
+  if (TestComm().Rank() != 0) return;
+
+  using id_set = ovk::id_set<2>;
+
+  id_set IDSet = {{1,2}, {1,3}};
+  EXPECT_EQ(IDSet.Count(), 2);
+
+}
+
+TEST_F(IDSetTests, Empty) {
+
+  if (TestComm().Rank() != 0) return;
+
+  using id_set = ovk::id_set<2>;
+
+  // Empty
+  {
+    id_set IDSet;
+    EXPECT_TRUE(IDSet.Empty());
+  }
+
+  // Not empty
+  {
+    id_set IDSet = {{1,2}, {1,3}};
+    EXPECT_FALSE(IDSet.Empty());
+  }
+
+}
+
+TEST_F(IDSetTests, Capacity) {
+
+  if (TestComm().Rank() != 0) return;
+
+  using id_set = ovk::id_set<2>;
+  using helper = ovk::core::test_helper<id_set>;
+
+  id_set IDSet = {{1,2}, {1,3}};
+  auto &Values = helper::GetValues(IDSet);
+  EXPECT_EQ(IDSet.Capacity(), Values.Capacity());
+
+}
+
+TEST_F(IDSetTests, BracketOperator) {
+
+  if (TestComm().Rank() != 0) return;
+
+  using id_set = ovk::id_set<2>;
+  using helper = ovk::core::test_helper<id_set>;
+
+  id_set IDSet = {{1,2}, {1,3}};
+  auto &Values = helper::GetValues(IDSet);
+  EXPECT_EQ(&IDSet[1], Values.Data(1));
+
+}
+
+TEST_F(IDSetTests, Data) {
+
+  if (TestComm().Rank() != 0) return;
+
+  using id_set = ovk::id_set<2>;
+  using helper = ovk::core::test_helper<id_set>;
+
+  id_set IDSet = {{1,2}, {1,3}};
+  auto &Values = helper::GetValues(IDSet);
+  EXPECT_EQ(IDSet.Data(), Values.Data());
+
+}
+
+TEST_F(IDSetTests, BeginEnd) {
+
+  if (TestComm().Rank() != 0) return;
+
+  using id_set = ovk::id_set<2>;
+  using helper = ovk::core::test_helper<id_set>;
+
+  id_set IDSet = {{1,2}, {1,3}};
+  auto &Values = helper::GetValues(IDSet);
+  EXPECT_EQ(IDSet.Begin(), Values.Data());
+  EXPECT_EQ(IDSet.End(), Values.Data()+2);
+
+  int Sum = 0;
+  for (auto &Value : IDSet) Sum += Value(0)+Value(1);
+  EXPECT_EQ(Sum, 7);
+
+}
+
+TEST_F(IDSetTests, Less) {
+
+  if (TestComm().Rank() != 0) return;
+
+  using id_set = ovk::id_set<2>;
+
+  // Equal
+  {
+    EXPECT_FALSE(id_set::Less({1,2}, {1,2}));
+  }
+
+  // Same first index, second index less
+  {
+    EXPECT_TRUE(id_set::Less({1,1}, {1,2}));
+  }
+
+  // Same first index, second index greater
+  {
+    EXPECT_FALSE(id_set::Less({1,3}, {1,2}));
+  }
+
+  // First index less
+  {
+    EXPECT_TRUE(id_set::Less({0,2}, {1,2}));
+  }
+
+  // First index greater
+  {
+    EXPECT_FALSE(id_set::Less({2,2}, {1,2}));
+  }
+
+}
