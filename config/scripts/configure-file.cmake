8bc14148bde2fd2011769f363065974d47cf482f
Matthew Smith <mjsmith6@illinois.edu>
2019-06-14 16:33:54 -0500

    updated license headers

--- config/scripts/configure-file.cmake
+++ config/scripts/configure-file.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2018 Matthew J. Smith and Overkit contributors
+# Copyright (c) 2019 Matthew J. Smith and Overkit contributors
 # License: MIT (http://opensource.org/licenses/MIT)
 
 configure_file(${CONFIG_IN} ${CONFIG_OUT} ${CONFIG_OPTIONS})
--- tests/MPIPrinter.hpp
+++ tests/MPIPrinter.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Matthew J. Smith and Overkit contributors
+// Copyright (c) 2019 Matthew J. Smith and Overkit contributors
 // License: MIT (http://opensource.org/licenses/MIT)
 
 // Parts of code below are adapted from Google Test's source code.
