f2243ba27caf696c5f186cc985c69c775f03b300
Matthew Smith <mjsmith6@illinois.edu>
2019-06-15 19:35:43 -0500

    added C++ API header

---
+++ src/overkit.hpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2019 Matthew J. Smith and Overkit contributors
+// License: MIT (http://opensource.org/licenses/MIT)
+
+#ifndef OVERKIT_HPP_INCLUDED
+#define OVERKIT_HPP_INCLUDED
+
+#include <ovk/core/all.hpp>
+#include <ovk/core-c/all.h>
+#include <ovk/extras/all.hpp>
+#include <ovk/extras-c/all.h>
+
+#endif
