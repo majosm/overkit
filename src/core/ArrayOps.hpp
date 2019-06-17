2ea8461ac7344e94fd52f604a6fd796840c9c3a9
Matthew Smith <mjsmith6@illinois.edu>
2019-06-17 09:36:17 -0500

    don't construct array_view in array ops (too much overhead for small arrays)

--- src/core/ArrayOps.hpp
+++ src/core/ArrayOps.hpp
@@ -17,38 +17,82 @@ namespace ovk {
 
 template <typename T, int Rank, array_layout Layout, OVK_FUNCTION_REQUIRES(!std::is_const<T>::value
   )> void ArrayFill(array_view<T, Rank, Layout> View, const T &Value) {
+
   View.Fill(Value);
+
 }
 
 template <typename ArrayType, OVK_FUNCTION_REQUIRES(core::IsArray<ArrayType>())> void ArrayFill(
   ArrayType &Array, const core::array_value_type<ArrayType> &Value) {
-  MakeArrayView(Array).Fill(Value);
+
+  long long NumValues = core::ArrayCount(Array);
+
+  for (long long i = 0; i < NumValues; ++i) {
+    Array[i] = Value;
+  }
+
+}
+
+template <typename T, int Rank, array_layout Layout, OVK_FUNCTION_REQUIRES(!std::is_const<T>::value
+  )> void ArrayFill(array_view<T, Rank, Layout> View, std::initializer_list<T> ValuesList) {
+
+  View.Fill(ValuesList);
+
+}
+
+template <typename ArrayType, OVK_FUNCTION_REQUIRES(core::IsArray<ArrayType>())> void ArrayFill(
+  ArrayType &Array, std::initializer_list<core::array_value_type<ArrayType>> ValuesList) {
+
+  long long NumValues = core::ArrayCount(Array);
+
+  auto Iter = ValuesList.begin();
+  for (long long i = 0; i < NumValues; ++i) {
+    Array[i] = *Iter++;
+  }
+
 }
 
 template <typename T, int Rank, array_layout Layout, typename IterType, OVK_FUNCTION_REQUIRES(
   !std::is_const<T>::value && core::IsInputIterator<IterType>() && std::is_convertible<
   core::iterator_reference_type<IterType>, T>::value)> void ArrayFill(array_view<T, Rank, Layout>
   View, IterType First) {
+
   View.Fill(First);
+
 }
 
 template <typename ArrayType, typename IterType, OVK_FUNCTION_REQUIRES(core::IsArray<ArrayType>() &&
   core::IsInputIterator<IterType>() && std::is_convertible<core::iterator_reference_type<IterType>,
   core::array_value_type<ArrayType>>::value)> void ArrayFill(ArrayType &Array, IterType First) {
-  MakeArrayView(Array).Fill(First);
+
+  long long NumValues = core::ArrayCount(Array);
+
+  IterType Iter = First;
+  for (long long i = 0; i < NumValues; ++i) {
+    Array[i] = *Iter++;
+  }
+
 }
 
 template <typename T, typename U, int Rank, array_layout Layout, OVK_FUNCTION_REQUIRES(
   !std::is_const<T>::value && std::is_convertible<typename std::remove_const<U>::type, T>::value)>
   void ArrayFill(array_view<T, Rank, Layout> View, array_view<U, Rank, Layout> SourceView) {
+
   View.Fill(SourceView);
+
 }
 
 template <typename ArrayType, typename T, int Rank, array_layout Layout, OVK_FUNCTION_REQUIRES(
   core::IsArray<ArrayType>() && core::ArrayHasFootprint<ArrayType, Rank, Layout>() &&
   std::is_convertible<typename std::remove_const<T>::type, core::array_value_type<ArrayType>>::
   value)> void ArrayFill(ArrayType &Array, array_view<T, Rank, Layout> SourceView) {
-  MakeArrayView(Array).Fill(SourceView);
+
+  long long NumValues = core::ArrayCount(Array);
+
+  for (long long i = 0; i < NumValues; ++i) {
+    Array[i] = SourceView[i];
+  }
+
 }
 
 template <typename T, int Rank, array_layout Layout, typename SourceArrayRefType,
@@ -57,7 +101,9 @@ template <typename T, int Rank, array_layout Layout, typename SourceArrayRefType
   && core::ArrayHasFootprint<typename std::decay<SourceArrayRefType>::type, Rank, Layout>() &&
   std::is_convertible<core::array_access_type<SourceArrayRefType &&>, T>::value)> void ArrayFill(
   array_view<T, Rank, Layout> View, SourceArrayRefType &&SourceArray) {
+
   View.Fill(std::forward<SourceArrayRefType>(SourceArray));
+
 }
 
 template <typename ArrayType, typename SourceArrayRefType, OVK_FUNCTION_REQUIRES(core::IsArray<
@@ -66,7 +112,13 @@ template <typename ArrayType, typename SourceArrayRefType, OVK_FUNCTION_REQUIRES
   typename std::decay<SourceArrayRefType>::type>() && std::is_convertible<core::array_access_type<
   SourceArrayRefType &&>, core::array_value_type<ArrayType>>::value)> void ArrayFill(ArrayType
   &Array, SourceArrayRefType &&SourceArray) {
-  MakeArrayView(Array).Fill(std::forward<SourceArrayRefType>(SourceArray));
+
+  long long NumValues = core::ArrayCount(Array);
+
+  for (long long i = 0; i < NumValues; ++i) {
+    Array[i] = static_cast<core::array_access_type<SourceArrayRefType &&>>(SourceArray[i]);
+  }
+
 }
 
 }
