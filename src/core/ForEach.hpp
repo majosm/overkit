064a2288cbf9e6afab13540c589e30d73a1f0a90
Matthew Smith <mjsmith6@illinois.edu>
2019-06-28 14:03:50 -0500

    minor change to ForEach

--- src/core/ForEach.hpp
+++ src/core/ForEach.hpp
@@ -11,6 +11,7 @@
 #include <ovk/core/TypeTraits.hpp>
 
 #include <type_traits>
+#include <utility>
 
 namespace ovk {
 namespace core {
@@ -30,66 +31,66 @@ template <typename FRef, typename T, int N> constexpr bool IsCallableWithTupleEl
 
 template <int N, int I, array_layout Layout> struct helper;
 template <int N, int I> struct helper<N, I, array_layout::ROW_MAJOR> {
-  template <typename T, typename F, typename... TupleElementTypes> static OVK_FORCE_INLINE void
-    ForEach(const interval<T,N> &Interval, F &&Func, TupleElementTypes... TupleElements) {
+  template <typename T, typename FRef, typename... TupleElementTypes> static OVK_FORCE_INLINE void
+    ForEach(const interval<T,N> &Interval, FRef &&Func, TupleElementTypes... TupleElements) {
     for (T i = Interval.Begin(N-1-I); i < Interval.End(N-1-I); ++i) {
-      helper<N, I-1, array_layout::ROW_MAJOR>::ForEach(Interval, std::forward<F>(Func),
+      helper<N, I-1, array_layout::ROW_MAJOR>::ForEach(Interval, std::forward<FRef>(Func),
         TupleElements..., i);
     }
   }
 };
 template <int N, int I> struct helper<N, I, array_layout::COLUMN_MAJOR> {
-  template <typename T, typename F, typename... TupleElementTypes> static OVK_FORCE_INLINE void
-    ForEach(const interval<T,N> &Interval, F &&Func, TupleElementTypes... TupleElements) {
+  template <typename T, typename FRef, typename... TupleElementTypes> static OVK_FORCE_INLINE void
+    ForEach(const interval<T,N> &Interval, FRef &&Func, TupleElementTypes... TupleElements) {
     for (T i = Interval.Begin(I); i < Interval.End(I); ++i) {
-      helper<N, I-1, array_layout::COLUMN_MAJOR>::ForEach(Interval, std::forward<F>(Func),
+      helper<N, I-1, array_layout::COLUMN_MAJOR>::ForEach(Interval, std::forward<FRef>(Func),
         i, TupleElements...);
     }
   }
 };
 template <int N> struct helper<N, 0, array_layout::ROW_MAJOR> {
-  template <typename T, typename F, typename... TupleElementTypes, OVK_FUNCTION_REQUIRES(
-    IsCallableWithTupleElements<F &&, T, N>())> static OVK_FORCE_INLINE void ForEach(const
-    interval<T,N> &Interval, F &&Func, TupleElementTypes... TupleElements) {
+  template <typename T, typename FRef, typename... TupleElementTypes, OVK_FUNCTION_REQUIRES(
+    IsCallableWithTupleElements<FRef &&, T, N>())> static OVK_FORCE_INLINE void ForEach(const
+    interval<T,N> &Interval, FRef &&Func, TupleElementTypes... TupleElements) {
     for (T i = Interval.Begin(N-1); i < Interval.End(N-1); ++i) {
-      Func(TupleElements...,i);
+      std::forward<FRef>(Func)(TupleElements...,i);
     }
   }
-  template <typename T, typename F, typename... TupleElementTypes, OVK_FUNCTION_REQUIRES(
-    !IsCallableWithTupleElements<F &&, T, N>() && IsCallableWith<F &&, const elem<T,N> &>())>
-    static OVK_FORCE_INLINE void ForEach(const interval<T,N> &Interval, F &&Func,
+  template <typename T, typename FRef, typename... TupleElementTypes, OVK_FUNCTION_REQUIRES(
+    !IsCallableWithTupleElements<FRef &&, T, N>() && IsCallableWith<FRef &&, const elem<T,N> &>())>
+    static OVK_FORCE_INLINE void ForEach(const interval<T,N> &Interval, FRef &&Func,
     TupleElementTypes... TupleElements) {
     for (T i = Interval.Begin(N-1); i < Interval.End(N-1); ++i) {
-      Func(elem<T,N>(TupleElements...,i));
+      std::forward<FRef>(Func)(elem<T,N>(TupleElements...,i));
     }
   }
 };
 template <int N> struct helper<N, 0, array_layout::COLUMN_MAJOR> {
-  template <typename T, typename F, typename... TupleElementTypes, OVK_FUNCTION_REQUIRES(
-    IsCallableWithTupleElements<F &&, T, N>())> static OVK_FORCE_INLINE void ForEach(const
-    interval<T,N> &Interval, F &&Func, TupleElementTypes... TupleElements) {
+  template <typename T, typename FRef, typename... TupleElementTypes, OVK_FUNCTION_REQUIRES(
+    IsCallableWithTupleElements<FRef &&, T, N>())> static OVK_FORCE_INLINE void ForEach(const
+    interval<T,N> &Interval, FRef &&Func, TupleElementTypes... TupleElements) {
     for (T i = Interval.Begin(0); i < Interval.End(0); ++i) {
-      Func(i,TupleElements...);
+      std::forward<FRef>(Func)(i,TupleElements...);
     }
   }
-  template <typename T, typename F, typename... TupleElementTypes, OVK_FUNCTION_REQUIRES(
-    !IsCallableWithTupleElements<F &&, T, N>() && IsCallableWith<F &&, const elem<T,N> &>())>
-    static OVK_FORCE_INLINE void ForEach(const interval<T,N> &Interval, F &&Func,
+  template <typename T, typename FRef, typename... TupleElementTypes, OVK_FUNCTION_REQUIRES(
+    !IsCallableWithTupleElements<FRef &&, T, N>() && IsCallableWith<FRef &&, const elem<T,N> &>())>
+    static OVK_FORCE_INLINE void ForEach(const interval<T,N> &Interval, FRef &&Func,
     TupleElementTypes... TupleElements) {
     for (T i = Interval.Begin(0); i < Interval.End(0); ++i) {
-      Func(elem<T,N>(i,TupleElements...));
+      std::forward<FRef>(Func)(elem<T,N>(i,TupleElements...));
     }
   }
 };
 
 }
 
-template <array_layout Layout=array_layout::ROW_MAJOR, typename T, int N, typename F,
-  OVK_FUNCTION_REQUIRES(IsCallableWith<F &&, const elem<T,N> &>() || for_each_internal::
-  IsCallableWithTupleElements<F &&, T, N>())> OVK_FORCE_INLINE void ForEach(const interval<T,N>
-  &Interval, F &&Func) {
+template <array_layout Layout=array_layout::ROW_MAJOR, typename T, int N, typename FRef,
+  OVK_FUNCTION_REQUIRES(IsCallableWith<FRef &&, const elem<T,N> &>() || for_each_internal::
+  IsCallableWithTupleElements<FRef &&, T, N>())> OVK_FORCE_INLINE void ForEach(const interval<T,N>
+  &Interval, FRef &&Func) {
   // Have to use N-1 and count down because we can specialize on (N, 0) but not (N, N-1)
-  return for_each_internal::helper<N, N-1, Layout>::ForEach(Interval, std::forward<F>(Func));
+  return for_each_internal::helper<N, N-1, Layout>::ForEach(Interval, std::forward<FRef>(Func));
 }
 
 }}
