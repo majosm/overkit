313ed21e0f59b765b0682c340b5e6cc30b2dc2ba
Matthew Smith <mjsmith6@illinois.edu>
2019-07-16 10:37:01 -0500

    added missing license header

--- src/core/IteratorTraits.hpp
+++ src/core/IteratorTraits.hpp
@@ -1,3 +1,6 @@
+// Copyright (c) 2019 Matthew J. Smith and Overkit contributors
+// License: MIT (http://opensource.org/licenses/MIT)
+
 #ifndef OVK_CORE_ITERATOR_TRAITS_HPP_LOADED
 #define OVK_CORE_ITERATOR_TRAITS_HPP_LOADED
 
