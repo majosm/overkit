1570b0b247a7a9943fe7f2a34450c3c8de19bb95
Matthew Smith <mjsmith6@illinois.edu>
2019-08-27 13:07:29 -0500

    oops

--- tests/fixtures/Interface.cpp
+++ tests/fixtures/Interface.cpp
@@ -1,8 +1,7 @@
 // Copyright (c) 2019 Matthew J. Smith and Overkit contributors
 // License: MIT (http://opensource.org/licenses/MIT)
 
-#ifndef OVK_TESTS_FIXTURES_INTERFACE_HPP_LOADED
-#define OVK_TESTS_FIXTURES_INTERFACE_HPP_LOADED
+#include "tests/fixtures/Interface.hpp"
 
 #include "support/Decomp.hpp"
 
@@ -451,5 +450,3 @@ ovk::domain Interface3DManualConnectivity(const ovk::comm &Comm, const ovk::box 
 }
 
 }
-
-#endif
